61d195eed274c6f0a8b708138fdeea9eb68759df
Haruki Nguyen <nmd03pvt@gmail.com>
2023-07-29 11:13:18 +0700

    feat: `bashrc_custom.sh` > add scripts for updating packages in Ubuntu and Arch-based distros

--- configs/bash/bashrc_custom.sh
+++ configs/bash/bashrc_custom.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+# Check if the OS is Ubuntu or Arch Linux
+if type "pacman" >/dev/null 2>&1; then
+  function new-date() {
+    echo "Updating system with pacman..."
+    yay -Syu --noconfirm
+
+    echo "Removing unused packages..."
+    yay -Yc --noconfirm
+
+    echo "System update and cleanup complete!"
+
+    echo "Updating packages of npm..."
+    sudo npm update -g
+    echo "Updating packages of pnpm..."
+    pnpm update -g
+  }
+elif type "apt" >/dev/null 2>&1 || type "apt-get" >/dev/null 2>&1; then
+  function update_and_cleanup_system() {
+    echo "Updating system with apt..."
+    sudo apt update && sudo apt upgrade -y
+
+    echo "Removing unused packages..."
+    sudo apt autoremove -y
+
+    echo "System update and cleanup complete!"
+
+    echo "Updating packages of npm..."
+    sudo npm update -g
+
+    echo "Updating packages of pnpm..."
+    pnpm update -g
+  }
+else
+  echo "Unknown OS"
+fi
+
 # Prompt theme
 eval "$(starship init bash)"
 
