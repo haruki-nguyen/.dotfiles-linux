af74ba81df73ecfdeca7f4413fc78fea84454bdb
Haruki Nguyen <nmd03pvt@gmail.com>
2024-01-15 14:50:49 +0700

    style:  > format code

--- configs/neovim/configs/overrides.lua
+++ configs/neovim/configs/overrides.lua
@@ -1,45 +1,45 @@
 local M = {}
 
 M.treesitter = {
-	ensure_installed = {
-		"vim",
-		"lua",
-		"markdown",
-		"markdown_inline",
-	},
-	indent = {
-		enable = true,
-	},
+  ensure_installed = {
+    "vim",
+    "lua",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+  },
 }
 
 M.mason = {
-	ensure_installed = {
-		-- Lua stuff
-		"lua-language-server",
-		"stylua",
+  ensure_installed = {
+    -- Lua stuff
+    "lua-language-server",
+    "stylua",
 
-		-- Others
-		"prettier",
-		"eslint_d",
-		"typescript-language-server",
-		"css-lsp",
-	},
+    -- Others
+    "prettier",
+    "eslint_d",
+    "typescript-language-server",
+    "css-lsp",
+  },
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-	git = {
-		enable = true,
-	},
+  git = {
+    enable = true,
+  },
 
-	renderer = {
-		highlight_git = true,
-		icons = {
-			show = {
-				git = true,
-			},
-		},
-	},
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
 }
 
 return M
