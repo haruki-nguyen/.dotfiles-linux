017a34062663ae0921d7472db222d33e7fa7d50f
Haruki Nguyen <nmd03pvt@gmail.com>
2024-12-24 21:00:35 +0700

    feat(windows-configs): update Powershell configs
    Update `Update-System` function and add new python alias

--- configs/windows-apps/powershell7.ps1
+++ configs/windows-apps/powershell7.ps1
@@ -1,12 +1,43 @@
-# Imports
-Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
-
-# Functions
-Function refreshShell { . $PROFILE }
-
-# Aliases
-Set-Alias -Name refsh -Value refreshShell
-
-# Add PATH
-$env:EDITOR += "nvim"
+# IMPORTS
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+
+# UPDATE SYSTEM
+# Define paths to git repositories
+$projectPaths = @("")
+
+function Git-PullAll {
+    foreach ($path in $projectPaths) {
+        Write-Host "Going to $path"
+        Set-Location -Path $path
+        Write-Host "Pulling..."
+        git pull
+        Write-Host "Done!"
+        Write-Host "Going back..."
+        Set-Location -Path -
+    }
+}
+
+function Update-System {
+    # Check if Chocolatey and npm are available
+    if (Get-Command choco -ErrorAction SilentlyContinue) {
+        Write-Host "Updating system with Chocolatey..."
+        choco upgrade all -y
+
+        Write-Host "Updating PowerShell modules from Gallery..."
+        Get-InstalledModule | Update-Module
+
+        Write-Host "Pulling updates from the repositories"
+        Git-PullAll
+        Write-Host "Done!"
+    } else {
+        Write-Host "Chocolatey not installed. Install it first: https://chocolatey.org/install"
+    }
+}
+
+# ALIASES
+Set-Alias -Name refsh -Value refreshShell
+Set-Alias -Name py -Value python
+
+# ADD PATH
+$env:EDITOR = "nvim"
 
