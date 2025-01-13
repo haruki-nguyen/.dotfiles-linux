ff9b31bf3f99092760458dee263336524d32817f
Haruki Nguyen <nmd03pvt@gmail.com>
2025-01-13 17:44:13 +0700

    feat(windows-configs): update `Update-System` function and add aliases

--- configs/windows-apps/powershell7.ps1
+++ configs/windows-apps/powershell7.ps1
@@ -1,11 +1,8 @@
-# IMPORTS
-Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
-
 # UPDATE SYSTEM
 # Define paths to git repositories
 $projectPaths = @("C:\Users\nmdex\Documents\IoT-irrigation-system")
 
-function Git-PullAll {
+function Get-RepoUpdates {
     foreach ($path in $projectPaths) {
         Write-Host "Going to $path"
         Set-Location -Path $path
@@ -19,20 +16,74 @@ function Git-PullAll {
 
 function Update-System {
     # Check if Chocolatey and npm are available
-    if (Get-Command choco -ErrorAction SilentlyContinue) {
-        Write-Host "Updating system with Chocolatey..."
-        choco upgrade all -y
+    if (Get-Command scoop -ErrorAction SilentlyContinue) {
+        Write-Host "Updating system with Scoop..."
+        scoop update *
 
         Write-Host "Updating PowerShell modules from Gallery..."
         Get-InstalledModule | Update-Module
 
         Write-Host "Pulling updates from the repositories"
-        Git-PullAll
+        Get-RepoUpdates
         Write-Host "Done!"
-    } else {
-        Write-Host "Chocolatey not installed. Install it first: https://chocolatey.org/install"
+    }
+    else {
+        Write-Host "Scoop isn't installed."
     }
 }
 
 # ALIASES
+Set-Alias -Name open -Value explorer
 Set-Alias -Name py -Value python
+Set-Alias -Name t -Value New-Item
+
+function Update-Profile {
+    . $profile
+}
+
+Set-Alias -Name refresh -Value Update-Profile
+
+# Git alias
+function Invoke-GitStatus {
+    git status
+}
+
+function Invoke-GitLog {
+    git log --graph --oneline --decorate
+}
+
+function Invoke-GitAddAll {
+    git add .
+}
+
+function Invoke-GitAdd {
+    param(
+        [string[]]$Paths
+    )
+    git add $Paths
+}
+
+function Invoke-GitCommit {
+    git commit
+}
+
+function Invoke-GitPullRebase {
+    git pull --rebase
+}
+
+function Invoke-GitSync {
+    git pull --rebase
+    git push
+}
+function Invoke-GitCommitizen {
+    git cz
+}
+
+Set-Alias -Name gits -Value Invoke-GitStatus
+Set-Alias -Name gitl -Value Invoke-GitLog
+Set-Alias -Name gitaa -Value Invoke-GitAddAll
+Set-Alias -Name gita -Value Invoke-GitAdd
+Set-Alias -Name gitc -Value Invoke-GitCommit
+Set-Alias -Name gitpr -Value Invoke-GitPullRebase
+Set-Alias -Name gitsync -Value Invoke-GitSync
+Set-Alias -Name gitcz -Value Invoke-GitCommitizen
