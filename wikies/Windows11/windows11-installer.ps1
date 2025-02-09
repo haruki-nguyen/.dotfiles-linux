e30aaf648f3896ca97b40f5cac5244ffb2321e33
Haruki Nguyen <nmd03pvt@gmail.com>
2025-02-09 11:25:57 +0700

    docs(windows-configs): remove the Session software from Windows installer

--- wikies/Windows11/windows11-installer.ps1
+++ wikies/Windows11/windows11-installer.ps1
@@ -4,7 +4,7 @@ Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
 # Install software via Scoop
 # llvm: for using `clang-format` cmd
-scoop install git neovim python espanso yt-dlp ffmpeg lf notion obs-studio openshot nodejs firefox flow-launcher kicad autohotkey super-productivity gitleaks session discord draw.io gh llvm ripgrep mingw
+scoop install git neovim python espanso yt-dlp ffmpeg lf notion obs-studio openshot nodejs firefox flow-launcher kicad autohotkey super-productivity gitleaks discord draw.io gh llvm ripgrep mingw
 
 Write-Host "Install GnuWin32 and set on path"
 
