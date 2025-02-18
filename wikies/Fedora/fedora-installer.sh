951e2780565b16ca80e28ef92acdccd5a69e24e8
Haruki Nguyen <nmd03pvt@gmail.com>
2025-02-18 21:08:42 +0700

    feat: add rofi program launcher

--- wikies/Fedora/fedora-installer.sh
+++ wikies/Fedora/fedora-installer.sh
@@ -8,7 +8,8 @@ sudo dnf update -y || { echo "System update failed"; exit 1; }
 
 # Install necessary packages
 # `fuse` and `fuse-libs`: run `espanso` AppImage.
-sudo dnf install -y neovim tmux zip ripgrep nodejs npm gdb python3 yt-dlp ffmpeg obs-studio openshot firefox gitleaks gh llvm fuse fuse-libs || { echo "Package installation failed"; exit 1; }
+# `rofi`: program launcher replacement for the default `dmenu`.
+sudo dnf install -y neovim tmux zip ripgrep nodejs npm gdb python3 yt-dlp ffmpeg obs-studio openshot firefox gitleaks gh llvm fuse fuse-libs rofi || { echo "Package installation failed"; exit 1; }
 
 # Install Espanso
 # Create the $HOME/opt destination folder
