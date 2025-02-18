22a2409b80787b16f445daadb50e7e03ce312a2e
Haruki Nguyen <nmd03pvt@gmail.com>
2025-02-18 10:48:17 +0700

    refactor(linux-configs): remove unused Bash scripts

--- bash/bashrc_custom.sh
+++ bash/bashrc_custom.sh
@@ -3,12 +3,6 @@
 # Add system updating script
 source ~/.dotfiles/configs/linux-softwares/bash/update-system.sh
 
-# Add search and replace script
-source ~/.dotfiles/configs/linux-softwares/bash/search-replace.sh
-
-# Prompt theme
-# eval "$(starship init bash)"
-
 # Z directory jumper
 source ~/.dotfiles/configs/linux-softwares/bash/z.sh
 
@@ -17,7 +11,6 @@ alias md="mkdir -p"
 alias t="touch"
 alias refresh="source ~/.bashrc"
 alias py="python3"
-alias open="explorer.exe"
 
 # Git alias
 alias gits="git status"
@@ -27,9 +20,7 @@ alias gita="git add"
 alias gitc="git commit"
 alias gitpr="git pull --rebase"
 alias gitsync="git pull --rebase && git push"
-alias gitui="gitui -t catppuccin_mocha.ron"
-alias gitcz="git cz"
 
 # Add PATH
-export PATH="/home/haruki/.cargo/bin:/home/haruki/.local/bin:$PATH"
+export PATH="/home/haruki/.local/bin:$PATH"
 
