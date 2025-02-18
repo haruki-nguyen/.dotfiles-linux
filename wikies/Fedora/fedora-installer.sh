c7883ff11e38667ce79c2c13a01b2908f788a638
Haruki Nguyen <nmd03pvt@gmail.com>
2025-02-18 23:15:44 +0700

    feat(linux-configs): add btop

--- wikies/Fedora/fedora-installer.sh
+++ wikies/Fedora/fedora-installer.sh
@@ -11,7 +11,8 @@ sudo dnf update -y || { echo "System update failed"; exit 1; }
 # `rofi`: program launcher replacement for the default `dmenu`.
 # `lxappearance`: for modifying GTK themes.
 # `gtk-murrine-engine`: GTK Gruvbox theme dependencies.
-sudo dnf install -y neovim tmux zip ripgrep nodejs npm gdb python3 yt-dlp ffmpeg obs-studio openshot firefox gitleaks gh llvm fuse fuse-libs rofi lxappearance gtk-murrine-engine || { echo "Package installation failed"; exit 1; }
+# `btop`: task manager
+sudo dnf install -y neovim tmux zip ripgrep nodejs npm gdb python3 yt-dlp ffmpeg obs-studio openshot firefox gitleaks gh llvm fuse fuse-libs rofi lxappearance gtk-murrine-engine btop || { echo "Package installation failed"; exit 1; }
 
 # Install Espanso
 # Create the $HOME/opt destination folder
