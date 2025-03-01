6479c6e8f68a7eff84e69c9dd140a7027501c1c3
Haruki Nguyen <nmd03pvt@gmail.com>
2025-03-01 22:13:08 +0700

    feat: add ohmyposh prompt theme

--- powershell-configs.ps1
+++ powershell-configs.ps1
@@ -1,9 +1,15 @@
-# UPDATE SYSTEM
-# Define paths to git repositories
-$projectPaths = @("C:\Users\nmdex\Documents\Projects\IoT-irrigation-system", "C:\Users\nmdex\Documents\Projects\IoT-IS-circuit-design", "C:\Users\nmdex\Documents\Projects\University")
-
-function Get-RepoUpdates {
-    foreach ($path in $projectPaths) {
+# UPDATE SYSTEM
+
+# Define paths to git repositories
+
+$projectPaths = @("C:\Users\nmdex\Documents\Projects\IoT-irrigation-system", "C:\Users\nmdex\Documents\Projects\IoT-IS-circuit-design", "C:\Users\nmdex\Documents\Projects\University")
+
+
+
+function Get-RepoUpdates {
+
+    foreach ($path in $projectPaths) {
+
 
         if (-Not (Test-Path $path)) {
             Write-Host "Invalid path: $path"
@@ -25,11 +31,16 @@ function Get-RepoUpdates {
             Write-Host "Going back..."
             Set-Location -Path $path -ErrorAction SilentlyContinue
         }
-    }
-}
-
-function Update-System {
-    # Check if Chocolatey and npm are available
+    }
+
+}
+
+
+
+function Update-System {
+
+    # Check if Chocolatey and npm are available
+
     try {
         if (Get-Command scoop -ErrorAction Stop) {
             Write-Host "Updating system with Scoop..."
@@ -49,56 +60,111 @@ function Update-System {
     catch {
         Write-Host "An error occured: $_"
     }
-}
-
-# ALIASES
-Set-Alias -Name open -Value explorer
-Set-Alias -Name py -Value python
-Set-Alias -Name t -Value New-Item
-
-function Update-Profile {
-    . $profile
-}
-
-Set-Alias -Name refresh -Value Update-Profile
-
-# Git alias
-function Invoke-GitStatus {
-    git status
-}
-
-function Invoke-GitLog {
-    git log --graph --oneline --decorate
-}
-
-function Invoke-GitAddAll {
-    git add .
-}
-
-function Invoke-GitAdd {
-    param(
-        [string[]]$Paths
-    )
-    git add $Paths
-}
-
-function Invoke-GitCommit {
-    git commit
-}
-
-function Invoke-GitPullRebase {
-    git pull --rebase
-}
-
-function Invoke-GitSync {
-    git pull --rebase
-    git push
-}
-
-Set-Alias -Name gits -Value Invoke-GitStatus
-Set-Alias -Name gitl -Value Invoke-GitLog
-Set-Alias -Name gitaa -Value Invoke-GitAddAll
-Set-Alias -Name gita -Value Invoke-GitAdd
-Set-Alias -Name gitc -Value Invoke-GitCommit
-Set-Alias -Name gitpr -Value Invoke-GitPullRebase
-Set-Alias -Name gitsync -Value Invoke-GitSync
+}
+
+
+
+# ALIASES
+
+Set-Alias -Name open -Value explorer
+
+Set-Alias -Name py -Value python
+
+Set-Alias -Name t -Value New-Item
+
+
+
+function Update-Profile {
+
+    . $profile
+
+}
+
+
+
+Set-Alias -Name refresh -Value Update-Profile
+
+
+
+# Git alias
+
+function Invoke-GitStatus {
+
+    git status
+
+}
+
+
+
+function Invoke-GitLog {
+
+    git log --graph --oneline --decorate
+
+}
+
+
+
+function Invoke-GitAddAll {
+
+    git add .
+
+}
+
+
+
+function Invoke-GitAdd {
+
+    param(
+
+        [string[]]$Paths
+
+    )
+
+    git add $Paths
+
+}
+
+
+
+function Invoke-GitCommit {
+
+    git commit
+
+}
+
+
+
+function Invoke-GitPullRebase {
+
+    git pull --rebase
+
+}
+
+
+
+function Invoke-GitSync {
+
+    git pull --rebase
+
+    git push
+
+}
+
+
+
+Set-Alias -Name gits -Value Invoke-GitStatus
+
+Set-Alias -Name gitl -Value Invoke-GitLog
+
+Set-Alias -Name gitaa -Value Invoke-GitAddAll
+
+Set-Alias -Name gita -Value Invoke-GitAdd
+
+Set-Alias -Name gitc -Value Invoke-GitCommit
+
+Set-Alias -Name gitpr -Value Invoke-GitPullRebase
+
+Set-Alias -Name gitsync -Value Invoke-GitSync
+
+# Activate OhMyPosh
+oh-my-posh init pwsh --config C:\Users\nmdex\.dotfiles\.config\ohmyposh\ohmyposh.toml | Invoke-Expression
