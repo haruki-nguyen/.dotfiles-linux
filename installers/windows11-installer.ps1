f95dc12e02bca7f62614fbcefa6e6354efeaa8df
Haruki Nguyen <nmd03pvt@gmail.com>
2025-04-03 17:51:32 +0700

    chore: minor update to windows installer

--- installers/windows11-installer.ps1
+++ installers/windows11-installer.ps1
@@ -17,12 +17,11 @@ scoop bucket add extras
 # `llvm`: get `clang` to installing packages for Treesitter in Neovim
 # `ffmpeg`: dependency of `yt-dlp`
 # `fzf`: fuzzy finder
-# `mactype-np`: get macOS font render
 
 # Add scoop nonportable bucket
 scoop add nonportable
 
-scoop install obsidian flow-launcher everything firefox powertoys neovim nodejs ripgrep mingw python espanso llvm yt-dlp ffmpeg notion obs-studio gitleaks discord draw.io gh wget fzf vscode freecad oh-my-posh mactype-np windirstat
+scoop install obsidian flow-launcher everything firefox powertoys neovim nodejs ripgrep mingw python espanso llvm yt-dlp ffmpeg notion obs-studio gitleaks discord draw.io gh wget fzf vscode freecad oh-my-posh windirstat
 
 # SET UP CONFIGS
 # Set location to HOME to clone repositories
